dfb4e3336b2b87abf1ff5e56420ce689565078a9
AzazelunVeliar <144543807+AzazelunVeliar@users.noreply.github.com>
2025-06-11 22:12:45 +0300

    UPD 1

--- cypress/e2e/constructor.cy.tsx
+++ cypress/e2e/constructor.cy.tsx
@@ -1,126 +1,40 @@
-const fixturesArr = ['user', 'order', 'ingredients', 'feeds'];
-//моки ендпоинтов апишки
-const intercepts = [
-  {
-    method: 'GET',
-    url: '/api/ingredients',
-    fixture: 'ingredients',
-    alias: 'ingredients'
-  },
-  {
-    method: 'GET',
-    url: '/api/auth/user',
-    fixture: 'user',
-    alias: 'user'
-  },
-  {
-    method: 'GET',
-    url: '/api/orders/all',
-    fixture: 'user-orders',
-    alias: 'user-orders'
-  },
-  {
-    method: 'GET',
-    url: '/api/feeds',
-    fixture: 'feeds',
-    alias: 'feeds'
-  },
-  {
-    method: 'POST',
-    url: '/api/orders',
-    fixture: 'order',
-    alias: 'order'
-  }
-];
+import { SELECTORS } from '../support/selectors';
 
 describe('Конструктор', () => {
   beforeEach(() => {
-    fixturesArr.forEach((fixture) => cy.fixture(`${fixture}.json`));
-    intercepts.forEach(({ method, url, fixture, alias }) =>
-      cy.intercept({ method, url }, { fixture }).as(alias)
-    );
-
-    cy.setCookie('accessToken', 'mymocktoken');
-    localStorage.setItem('refreshToken', 'mymocktoken');
-
     cy.visit('/');
+    cy.clickFirstMenuIngredient();
   });
 
-  afterEach(() => {
-    cy.clearCookie('accessToken');
-    localStorage.removeItem('refreshToken');
+  it('Открытие модалки ингредиента', () => {
+    cy.get(SELECTORS.MODAL)
+      .should('be.visible')
+      .should('contain.text', 'Краторная булка N-200i');
   });
 
-  it('Главная страница', () => {
-    cy.visit('/');
+  it('Закрытие кликом по оверлею', () => {
+    cy.get('[data-cy="modal-overlay"]').click({ force: true });
+    cy.assertModalNotExist();
   });
 
-  it('Моки эндпоинтов работают', () => {
-    cy.wait(['@ingredients', '@user']);
+  it('Закрытие крестиком', () => {
+    cy.get('[data-cy="modal-close"]').click();
+    cy.assertModalNotExist();
   });
 
-  describe('Функционал модалки', () => {
-    beforeEach(() => {
-      cy.get('[data-cy="menu-ingredient"]').first().click();
-    });
-
-    it('Открытие', () => {
-      cy.get('[data-cy="modal"]')
-        .should('be.visible')
-        .should('contain.text', 'Краторная булка N-200i');
-    });
-
-    it('Закрытие кликом по оверлею', () => {
-      cy.get('[data-cy="modal-overlay"]').click({ force: true });
-      cy.get('[data-cy="modal"]').should('not.exist');
-    });
-
-    it('Закрытие при нажатии кнопки', () => {
-      cy.get('[data-cy="modal-close"]').click();
-      cy.get('[data-cy="modal"]').should('not.exist');
-    });
-  });
-
-  describe('Добавление разных ингредиентов', () => {
-    it('Булка', () => {
-      cy.get('[data-cy="menu-ingredient"]').first().children().last().click();
-      cy.get('[data-cy="constructor-bun-top"]').should('be.visible');
-      cy.get('[data-cy="constructor-bun-bottom"]').should('be.visible');
-    });
-
-    it('Начинка', () => {
-      cy.get('[data-cy="menu-ingredient"]').last().children().last().click();
-      cy.get('[data-cy="constructor-ingredient"]').should('be.visible');
-    });
-
-    it('Полная сборка и заказ', () => {
-      cy.get('[data-cy="menu-ingredient"]').first().children().last().click();
-      cy.get('[data-cy="constructor-bun-top"]').should('be.visible');
-      cy.get('[data-cy="constructor-bun-bottom"]').should('be.visible');
-
-      cy.get('[data-cy="menu-ingredient"]').last().children().last().click();
-      cy.get('[data-cy="constructor-ingredient"]').should('be.visible');
-
-      cy.get('[data-cy="constructor-order-button"]').click();
-
-      cy.wait('@order').then((res) => {
-        const orderNumber = res.response?.body?.order?.number;
-
-        cy.get('[data-cy="modal"]', { timeout: 15000 })
-          .should('be.visible')
-          .as('modal');
-
-        cy.get('[data-cy="order_number"]', { timeout: 15000 })
-          .should('be.visible')
-          .should('contain.text', orderNumber);
-
-        cy.get('[data-cy="modal"]').find('button').click();
-        cy.get('[data-cy="modal"]').should('not.exist');
-
-        cy.get('[data-cy="constructor-bun-top"]').should('not.exist');
-        cy.get('[data-cy="constructor-ingredient"]').should('not.exist');
-        cy.get('[data-cy="constructor-bun-bottom"]').should('not.exist');
-      });
-    });
+  it('Добавление ингредиентов и оформление заказа', () => {
+    cy.addFirstMenuIngredient();
+    cy.get(SELECTORS.CONSTRUCTOR_BUN_TOP).should('be.visible');
+    cy.get(SELECTORS.CONSTRUCTOR_BUN_BOTTOM).should('be.visible');
+    cy.addLastMenuIngredient();
+    cy.get(SELECTORS.CONSTRUCTOR_INGREDIENT).should('be.visible');
+    cy.get('[data-cy="constructor-order-button"]').click();
+    cy.get(SELECTORS.MODAL, { timeout: 15000 }).should('be.visible').as('modal');
+    cy.get('[data-cy="order_number"]', { timeout: 15000 })
+      .should('be.visible')
+      .should('contain.text', orderNumber);
+    cy.closeModal();
+    cy.assertModalNotExist();
+    cy.assertConstructorIsEmpty();
   });
 });
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,37 +1,39 @@
 /// <reference types="cypress" />
-// ***********************************************
-// This example commands.ts shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
+import { SELECTORS } from './selectors';
+
+Cypress.Commands.add('setupIntercepts', () => {
+  const intercepts = [
+    { method: 'GET',  url: '/api/ingredients',    fixture: 'ingredients', alias: 'ingredients' },
+    { method: 'GET',  url: '/api/auth/user',      fixture: 'user',        alias: 'user' },
+    { method: 'GET',  url: '/api/orders/all',     fixture: 'user-orders', alias: 'user-orders' },
+    { method: 'GET',  url: '/api/feeds',          fixture: 'feeds',       alias: 'feeds' },
+    { method: 'POST', url: '/api/orders',         fixture: 'order',       alias: 'order' },
+  ];
+
+  intercepts.forEach(({ method, url, fixture, alias }) => {
+    cy.intercept({ method, url: new RegExp(url) }, { fixture }).as(alias);
+  });
+});
+
+Cypress.Commands.add('clickFirstMenuIngredient', () => {
+  cy.get(SELECTORS.MENU_INGREDIENT).first().click();
+});
+Cypress.Commands.add('addFirstMenuIngredient', () => {
+  cy.get(SELECTORS.MENU_INGREDIENT).first().children().last().click();
+});
+Cypress.Commands.add('addLastMenuIngredient', () => {
+  cy.get(SELECTORS.MENU_INGREDIENT).last().children().last().click();
+});
+
+Cypress.Commands.add('closeModal', () => {
+  cy.get(SELECTORS.MODAL).find('button').click();
+});
+Cypress.Commands.add('assertModalNotExist', () => {
+  cy.get(SELECTORS.MODAL).should('not.exist');
+});
+
+Cypress.Commands.add('assertConstructorIsEmpty', () => {
+  cy.get(SELECTORS.CONSTRUCTOR_BUN_TOP).should('not.exist');
+  cy.get(SELECTORS.CONSTRUCTOR_INGREDIENT).should('not.exist');
+  cy.get(SELECTORS.CONSTRUCTOR_BUN_BOTTOM).should('not.exist');
+});
